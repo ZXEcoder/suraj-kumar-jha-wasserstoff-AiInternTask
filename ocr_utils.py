6e462f95c71119e6f00401fa57f6ee6c0d572593
Suraj Kumar Jha <117300003+ZXEcoder@users.noreply.github.com>
2024-08-19 17:04:42 +0530

    Update ocr_utils.py
    given a comment to pytesseract_cmd so that it can be deployed easily in hugging facr

--- ocr_utils.py
+++ ocr_utils.py
@@ -8,12 +8,13 @@ def preprocess_image_for_ocr(image_path):
     return image
 
 def extract_text_from_object(image_path):
-    pytesseract.pytesseract.tesseract_cmd = r'/usr/bin/tesseract'
+    #use below command only if you have linux machine or else use windows command else if you want to deploy in huggingface don't use it
+    #pytesseract.pytesseract.tesseract_cmd = r'/usr/bin/tesseract'
     preprocessed_image = preprocess_image_for_ocr(image_path)
     text = pytesseract.image_to_string(preprocessed_image)
     return text
 def extract_image_as_whole(image_path):
-    pytesseract.pytesseract.tesseract_cmd = r'/usr/bin/tesseract'
+    #pytesseract.pytesseract.tesseract_cmd = r'/usr/bin/tesseract'
     image = cv2.imread(image_path)
     txt= pytesseract.image_to_string(image)
     return txt
